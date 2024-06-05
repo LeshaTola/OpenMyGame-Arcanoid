e1a31206f8efcba838e50f5fca9697a5fbcae182
LeshaTola <tola.lesha@mail.ru>
2024-06-05 11:35:59 +0300

    Fix Additing new packs

--- Assets/App/Scripts/Scenes/PackSelection/Feature/Packs/UI/PackMenu.cs
+++ Assets/App/Scripts/Scenes/PackSelection/Feature/Packs/UI/PackMenu.cs
@@ -2,8 +2,8 @@
 using Module.Localization;
 using Scenes.PackSelection.Feature.Packs.Configs;
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Zenject;
 
@@ -25,16 +25,39 @@ namespace Scenes.PackSelection.Feature.Packs.UI
 			this.localizationSystem = localizationSystem;
 		}
 
-		public void GeneratePackList(IEnumerable packs, Dictionary<string, SavedPackData> packsData)
+		public void GeneratePackList(List<Pack> packs, Dictionary<string, SavedPackData> packsData)
 		{
 			foreach (Pack pack in packs)
 			{
+
 				PackUI packUI = packFactory.GetPackUI();
-				packUI.UpdateUI(pack, packsData[pack.Id], localizationSystem);
+				SavedPackData packData = GetPackData(packsData, packs, pack);
+				packUI.UpdateUI(pack, packData, localizationSystem);
 				packUI.transform.SetParent(container);
 				packUI.transform.localScale = Vector3.one;
 				packUI.onPackClicked += () => OnPackSelected?.Invoke(pack);
 			}
+		}
+
+		private static SavedPackData GetPackData(Dictionary<string, SavedPackData> packsData, List<Pack> packs, Pack pack)
+		{
+			SavedPackData packData;
+			if (!packsData.ContainsKey(pack.Id))
+			{
+				packData = new SavedPackData()
+				{
+					Id = pack.Id,
+					CurrentLevel = 0,
+					IsCompeted = false,
+					IsOpened = packs.Last() == pack
+				};
+			}
+			else
+			{
+				packData = packsData[pack.Id];
+			}
+
+			return packData;
 		}
 	}
 }
