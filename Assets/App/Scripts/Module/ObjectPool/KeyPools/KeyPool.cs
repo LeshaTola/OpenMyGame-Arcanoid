a40c7aa36912df73c40d54fc33ab7bad0dc04781
LeshaTola <tola.lesha@mail.ru>
2024-06-10 13:59:26 +0300

    Add key pool Sort pools folder

---
+++ Assets/App/Scripts/Module/ObjectPool/KeyPools/KeyPool.cs
@@ -0,0 +1,31 @@
+﻿using Module.ObjectPool;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.App.Scripts.Module.ObjectPool.KeyPools
+{
+	public class KeyPool<T> where T : class
+	{
+		public Dictionary<string, IPool<T>> poolsDictionary;
+
+		public T Get(string key)
+		{
+			if (poolsDictionary.TryGetValue(key, out IPool<T> correctPool))
+			{
+				return correctPool.Get();
+			}
+
+			Debug.LogError($"There is no such pool key: {key}");
+			return null;
+		}
+
+		public void Release(string key, T pooledObject)
+		{
+			if (poolsDictionary.TryGetValue(key, out IPool<T> correctPool))
+			{
+				correctPool.Release(pooledObject);
+			}
+			Debug.LogError($"There is no such pool key: {key}");
+		}
+	}
+}
